20f714f974a8e2020cf301cfdff5763992d7de08
Doğacan Koç <74499668+Doxria@users.noreply.github.com>
2023-01-03 05:17:52 +0300

    🌍Turkish Language update!🌍
    🐌Turkish language option🐌

--- locales/tr.lua
+++ locales/tr.lua
@@ -1,1 +1,12 @@
+local Translations = {
+    notify = {
+        totals = 'Toplam kazanç : %{result} Sınır : %{limit}',
+        added = 'iş limitinize eklendi',
+        cantearn = "O kadar para kazanamazsın. Limit %{limit} $ ve şimdiden %{earning} $ kazandınız."
+    }
+}
 
+Lang = Locale:new({
+    phrases = Translations,
+    warnOnMissing = true
+})
